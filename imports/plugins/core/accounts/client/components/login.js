adb15bd8b54b37e79c3701589b1257e2b4af60c6
Dennis Wanjiru <denniswanjiru71@gmail.com>
2019-11-25 12:26:33 +0300

    feature: change reaction favicon to acdcs' black favicon

--- imports/plugins/core/accounts/client/components/login.js
+++ imports/plugins/core/accounts/client/components/login.js
@@ -65,6 +65,7 @@ class Login extends Component {
     const { currentView } = this.state;
     const isLogin = window.location.search.includes("signin");
     const isSignup = window.location.search.includes("signup");
+    const basePath = window.location.origin + window.location.pathname;
 
     if (currentView === "loginFormSignInView" || currentView === "loginFormSignUpView" || currentView === "loginFormUpdatePasswordView") {
       if (isOauthFlow) {
@@ -77,12 +78,12 @@ class Login extends Component {
             />
             {isLogin && <StyledAuthLink>
               <p>Don't have an account?</p>
-              <a className="auth-option" href="http://localhost:3000/account/login?action=signup&login_challenge=6722e384821e4c968c9272d1ea7db8c0">Sign Up</a>
+              <a className="auth-option" href={`${basePath}?action=signup&login_challenge=6722e384821e4c968c9272d1ea7db8c0`}>Sign Up</a>
             </StyledAuthLink>}
 
             {isSignup && <StyledAuthLink>
               <p>Already have an account?</p>
-              <a className="auth-option" href="http://localhost:3000/account/login?action=signin&login_challenge=6722e384821e4c968c9272d1ea7db8c0">Login</a>
+              <a className="auth-option" href={`${basePath}?action=signin&login_challenge=6722e384821e4c968c9272d1ea7db8c0`}>Login</a>
             </StyledAuthLink>}
           </StyledAuth>
         );
